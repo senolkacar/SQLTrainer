10e9deef69390a7300d7d5960af87895ce2f2a30
Kaçar Senol <senol.kacar@infrabel.be>
2025-06-30 13:35:25 +0200

    base of the sqltrainer project

---
+++ backend/src/test/java/com/senolkacar/sqltrainer/SqltrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senolkacar.sqltrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqltrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
